8b2230a358afb44e98b8d48c5b34be1558a7fea5
Sloan Nietert <sloan.nietert@gmail.com>
2018-07-18 12:49:29 -0400

    added comment to server settings for running locally

--- client/ts/serverSettings.ts
+++ client/ts/serverSettings.ts
@@ -1,2 +1,2 @@
-const SERVER_ENDPOINT = 'https://watt-center-snake.herokuapp.com';
+const SERVER_ENDPOINT = 'https://watt-center-snake.herokuapp.com'; // 'localhost:3000'
 export default SERVER_ENDPOINT;
